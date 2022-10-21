17318d1e7f7c1e49d6112781244737bef6b3ff17
Guilherme Correa <78979736+guilhermedcorrea@users.noreply.github.com>
2022-10-21 11:36:53 -0300

    Update controllers_notas.py

--- app/controllers/controllers_notas.py
+++ app/controllers/controllers_notas.py
@@ -3,7 +3,10 @@ import os
 from dotenv import load_dotenv
 import requests
 import json
-from typing import Dict, Tuple, List, Any
+from typing import Dict, Tuple, List, Literal, Any
+from ..extensions import db
+from itertools import groupby, chain
+from sqlalchemy import text
 
 """
 NOTA FISCAL CONSUMIDOR
@@ -13,6 +16,16 @@ load_dotenv()
 
 API_KEY_EMISSAO = os.getenv('API_KEY_EMISSAO')
 COMPANY_ID_EMISSAO = os.getenv('COMPANY_ID_EMISSAO')
+
+
+def converte_float(valores):
+    try:
+        num = str(valores).replace("."
+                ,"").replace(",",".").strip()
+        return float(num)
+    except:
+        return float(0)
+
 
 def get_metodo(f) -> Any:
     @wraps(f)
@@ -31,27 +44,35 @@ def get_metodo(f) -> Any:
 
         #return jsons
         return (kwargs.get('CodigoPedido'))
-        
     return obtem_endpoint
 
 def emissao_nfe(f) -> Any:
     @wraps(f)
     def emissao_notas_fiscais(*args: tuple, **kwargs: Dict[str, Any]) -> Any:
-
+        for listas in args:
+            for items in listas:
+                print(items.get('CodigoPedido'), items.get('IdCliente'),items.get('NomeCliente')
+                ,items.get('SKU'),items.get('QtdCaixa'),items.get('PrecoUnitario'), items.get('Quantidade')
+                ,items.get('EAN'), items.get('NCM'),items.get('Marca'), items.get('NomeProduto'),items.get('NomeCliente')
+                ,items.get('CpfCnpj'),items.get('ValorTotal'),items.get('Unidade')
+                ,items.get('RazaoSocialFranquiaVenda'),items.get('Bairro'),items.get('Celular')
+                ,items.get('Cep'),items.get('Complemento'),items.get('Endereco')
+                ,items.get('Desconto'), items.get('DescontoItem'),items.get('Nome'),items.get('Uf'))
+                        
         print('emissao notas fiscais ')
-
+        '''
         url = """https://api.nfse.io/v2/companies/{COMPANY_ID_EMISSAO}/productinvoices/""".format(COMPANY_ID_EMISSAO)
 
         payload = json.dumps({
         "buyer": {
-            "name": "{}","tradeName": "{}","address": {"city": {"code": "{}","name": "{}"
-            },"state": "SP","district": "distrito","street": "Alameda Madri","postalCode": "{}","number": "{}","country": "{}"},
-            "federalTaxNumber": {}
+            "name": "Teste NF TESTE","tradeName": "Comprador Nome Comercial","address": {"city": {"code": "1751488","name": "Marilia"
+            },"state": "SP","district": "distrito","street": "Alameda Madri","postalCode": "1751488","number": "555","country": "BRA"},
+            "federalTaxNumber": 99999999999999
         },
-        "items": [{"code": "{}","unitAmount": {},"quantity": {},"cfop": {}, "ncm": "{}","codeGTIN": "{}",
-            "codeTaxGTIN": "{}","tax": {"totalTax": {},"icms": {"csosn": "{}","origin": "{}"},"pis": { "amount": {},"rate": {},"baseTax": {}, "cst": "{}"
-                },"cofins": {"amount": {},"rate": {},"baseTax": {},"cst": "{}"}},"cest": "","description": "{}"
-            }]}).format()
+        "items": [{"code": "20968A","unitAmount": 87.9,"quantity": 33.9,"cfop": 5102, "ncm": "69072100","codeGTIN": "7894287914364",
+            "codeTaxGTIN": "7894287914364","tax": {"totalTax": 6,"icms": {"csosn": "102","origin": "0"},"pis": { "amount": 0,"rate": 0,"baseTax": 208,          "cst": "08"
+                },"cofins": {"amount": 0,"rate": 0,"baseTax": 208,"cst": "08"}},"cest": "","description": "TESTE DE PRODUTO - WITMOB"
+            }]})
         headers = {
         'Authorization': f'{API_KEY_EMISSAO}',
         'Content-Type': 'application/json'
@@ -60,8 +81,9 @@ def emissao_nfe(f) -> Any:
         response = requests.request("POST", url, headers=headers, data=payload)
 
         print(response.status_code)
- 
-        return response.json()
+        '''
+        #return response.json()
+        return 'teste'
         
     return emissao_notas_fiscais
 
@@ -94,18 +116,6 @@ def list_all_empresas(f) -> Any:
     return obtem_endpoint
 
 
-class NotasHausz:
-    def __init__(self, pedido, data, unidade, nf):
-        self.pedido = pedido
-        self.data = data
-        self.unidade = unidade
-        self.lista_dicts = []
-
-    def seleciona_fields(self, *args: tuple, **kwargs: dict[str, Any]) -> dict[str, Any]:pass
-
-    def get_jsons_api(self): pass
-
-    def emissao_nf(self) -> None: pass
 
        
 
